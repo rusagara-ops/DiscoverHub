74f5bbbc99b22b3b4017eb36ec797f721adb4b36
Kevin Rusagara Iraguha <kevin.rusagara@yale.edu>
2024-09-29 04:24:48 -0400

    displayed place lists

--- components/PlaceList.tsx
+++ components/PlaceList.tsx
@@ -4,11 +4,15 @@ import PlaceItemCard from './PlaceItemCard'
 
 function PlaceList({placeList}:any) {
   return (
-    <div className='px-[10px] md:px-[120px] mt-7'>
+    <div className='px-[10px] md:px-[120px] mt-7 z-10'>
         <h2 className='text-[20px] font-bold'>Search Results</h2>
-        <div>
+        <div className='
+        grid grid-cols-2 
+        md:grid-cols-3 
+        lg:grid-cols-4
+        gap-5'>
             {placeList.map((place:any,index:number)=>(
-                <div>
+                <div className='z-10' key={index}>
                     <PlaceItemCard place={place}/>
                 </div>
             ))}
